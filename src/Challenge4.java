f27457b8c500c29d3f02dc77116d108e5c292e03
Arthur Kroth <kroth.sc@gmail.com>
2023-09-09 08:41:04 +0100

    Challenge4

--- src/Challenge4.java
+++ src/Challenge4.java
@@ -1,6 +1,23 @@
+/*
+Leap year calculator
+Arthur Kroth
+9.9.2023
+*/
 public class Challenge4 {
     public static void main(String[] args) {
-
+        boolean leap = isLeapYear(1924);
+        System.out.println(leap);
+    }
+    public static boolean isLeapYear(int year){
+        if (year < 1 || year > 9999){
+            return false;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else if (year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
@@ -18,6 +35,7 @@ public class Challenge4 {
 //        1. If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
 //        2. If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
 //        3. If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+
 //        4. The year is a leap year (it has 366 days). The method isLeapYear needs to return true.
 //        5. The year is not a leap year (it has 365 days). The method isLeapYear needs to return false.
 //
